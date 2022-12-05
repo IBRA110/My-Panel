f44e3b161a8c51d95d24baaf9e230945007fa49a
Ibrahim <mamedovarbeiter@gmail.com>
2022-12-05 07:47:05 +0300

    created AccountController && register method

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+using System.Security.Cryptography;
+using System.Text;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        {
+            HMACSHA512 hmac = new HMACSHA512();
+            AppUser user = new AppUser
+            {
+                UserName = username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
