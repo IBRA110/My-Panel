a88221f64a717f642e0168e14a3c1f5acb86d78b
Ibrahim <mamedovarbeiter@gmail.com>
2022-12-11 02:45:47 +0300

    created login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -29,7 +29,7 @@ namespace API.Controllers
             AppUser user = new AppUser
             {
                 Id = Ulid.NewUlid(),
-                UserName = registerDTO.Username.ToLower(),
+                UserName = registerDTO.Username,
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
@@ -39,10 +39,32 @@ namespace API.Controllers
 
             return user;
         }
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            AppUser user = await _context.Users
+                .SingleOrDefaultAsync(x => x.UserName.ToLower() == loginDTO.Username.ToLower());
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+            
+            HMACSHA512 hmac = new HMACSHA512(user.PasswordSalt);
+            byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
 
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid password");
+                }
+            }
+
+            return user;
+        }
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
         }
     }
 }
