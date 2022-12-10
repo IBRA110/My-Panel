92dece626897d70031392a815d79ed54af57a1b3
Ibrahim <mamedovarbeiter@gmail.com>
2022-12-11 02:10:49 +0300

    added register DTO & created userExists method

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 using System.Security.Cryptography;
 using System.Text;
+using API.DTOs;
 using Core.Entities;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,14 +18,19 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
         {
+            if (await UserExists(registerDTO.Username))
+            {
+                return BadRequest("Username is taken");
+            }
+            
             HMACSHA512 hmac = new HMACSHA512();
             AppUser user = new AppUser
             {
                 Id = Ulid.NewUlid(),
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -31,6 +38,11 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
