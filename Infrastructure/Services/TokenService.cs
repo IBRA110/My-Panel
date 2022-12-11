4e369bf2e8f372f5323f8d6fdf6f5d4b5da4bb35
Ibrahim <mamedovarbeiter@gmail.com>
2022-12-11 18:37:06 +0300

    added the create token logic

--- Infrastructure/Services/TokenService.cs
+++ Infrastructure/Services/TokenService.cs
@@ -1,13 +1,41 @@
 using Core.Interfaces;
 using Core.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Infrastructure.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            SigningCredentials creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddMinutes(15),
+                SigningCredentials = creds
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
