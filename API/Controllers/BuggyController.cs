16ea5d437956c994cbc64606f1c4cae273cac58b
Ibrahim <mamedovarbeiter@gmail.com>
2022-12-15 18:46:06 +0300

    created an error controller for testing errors

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,51 @@
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+        
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("auth")]
+        [Authorize]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+        
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(new Ulid());
+            if (thing == null) 
+            {
+                return NotFound();
+            }
+            return Ok(thing);
+        }
+        
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+            // can return null!!!
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This was not a good request!");
+        }
+    }
+}
--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -6,7 +6,9 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
+        [StringLength(16, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
