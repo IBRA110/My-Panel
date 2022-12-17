9622d9f9df2df10ee7a665f37e1bde4a787aa77a
Ibrahim <mamedovarbeiter@gmail.com>
2022-12-18 01:19:52 +0300

    created email validation

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using API.Helpers;
 
 namespace API.DTOs
 {
@@ -10,11 +11,13 @@ namespace API.DTOs
         public string LastName { get; set; }
         
         [Required(ErrorMessage = "Email is required!")]
-        [EmailAddress(ErrorMessage = "Invalid email address!")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", 
+            ErrorMessage = "Please enter a valid e-mail address")]
         public string Email { get; set; }
         
         [Required(ErrorMessage = "Password is required!")]
         [StringLength(24, MinimumLength = 8, ErrorMessage = "Password must be greater than 8!")]
         public string Password { get; set; }
+
     }
 }
