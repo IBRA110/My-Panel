ff5b6e51583bbf2f41a3dd0833d8218ce0fa5b62
Ibrahim <mamedovarbeiter@gmail.com>
2022-12-23 21:30:00 +0300

    remove image

--- DrawMyDream.API/Controllers/ImageController.cs
+++ DrawMyDream.API/Controllers/ImageController.cs
@@ -12,7 +12,6 @@ namespace DrawMyDream.API.Controllers
         {
             try
             {
-
                 string FileName = file.FileName;
 
                 string uniqueFileName = Guid.NewGuid().ToString() + "_" + FileName;
