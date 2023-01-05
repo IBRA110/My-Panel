2338ab7ff50dd864b21de442567ebb35fba0d8cb
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-05 23:53:10 +0300

    code review && added themes.enum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from './core/theme/theme.service';
+import { LocalStorageKeysEnum } from './core/enums/local-storage-keys.enum';
+import { LocalStorageService } from './core/services/local-storage.service';
+import { ThemesEnum } from './core/enums/themes.enum';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +13,23 @@ import { ThemeService } from './core/theme/theme.service';
 export class AppComponent implements OnInit {
   private currentTheme: string | null = '';
 
-  public constructor(private themeService: ThemeService) {}
+  public constructor(
+    private themeService: ThemeService,
+    private localStorageService: LocalStorageService,
+  ) {}
 
   public ngOnInit(): void {
-    this.currentTheme = localStorage.getItem('Theme');
+    this.currentTheme = this.localStorageService.getData(
+      LocalStorageKeysEnum.CURRENT_THEME,
+    );
     if (!!this.currentTheme) {
       this.themeService.setTheme(this.currentTheme);
     } else {
-      localStorage.setItem('Theme', 'cosmic');
-      this.themeService.setTheme('cosmic');
+      this.localStorageService.saveData(
+        LocalStorageKeysEnum.CURRENT_THEME,
+        ThemesEnum.LIGHT,
+      );
+      this.themeService.setTheme(ThemesEnum.LIGHT);
     }
   }
 }
--- src/app/core/services/local-storage.service.ts
+++ src/app/core/services/local-storage.service.ts
@@ -4,14 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LocalStorageService {
-  public hasLocalStorage: boolean = false;
-
   public constructor() {}
 
   public saveData(key: string, data: any): void {
-    if (!this.hasLocalStorage) {
-      return;
-    }
     localStorage.setItem(key, data);
+  }
+
+  public getData(key: string): string | null {
+    return localStorage.getItem(key);
   }
 }
