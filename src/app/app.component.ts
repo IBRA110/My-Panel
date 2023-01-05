9a44d1f26c3710e31a34a8ca1bb9efd903dcd673
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-06 01:22:44 +0300

    removed unused methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ThemeService } from './core/theme/theme.service';
-import { LocalStorageKeysEnum } from './core/enums/local-storage-keys.enum';
-import { LocalStorageService } from './core/services/local-storage.service';
-import { ThemesEnum } from './core/enums/themes.enum';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,26 +6,4 @@ import { ThemesEnum } from './core/enums/themes.enum';
     <router-outlet></router-outlet>
   </div>`,
 })
-export class AppComponent implements OnInit {
-  private currentTheme: string | null = '';
-
-  public constructor(
-    private themeService: ThemeService,
-    private localStorageService: LocalStorageService,
-  ) {}
-
-  public ngOnInit(): void {
-    this.currentTheme = this.localStorageService.getData(
-      LocalStorageKeysEnum.CURRENT_THEME,
-    );
-    if (!!this.currentTheme) {
-      this.themeService.setTheme(this.currentTheme);
-    } else {
-      this.localStorageService.saveData(
-        LocalStorageKeysEnum.CURRENT_THEME,
-        ThemesEnum.LIGHT,
-      );
-      this.themeService.setTheme(ThemesEnum.LIGHT);
-    }
-  }
-}
+export class AppComponent {}
