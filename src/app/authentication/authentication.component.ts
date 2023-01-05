0c5998485f3e2ae7a88c126daf1808d13b621b66
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-05 21:11:02 +0300

    created authentication component && added new routes && created sign-in sign-up components

---
+++ src/app/authentication/authentication.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication',
+  templateUrl: `<router-outlet></router-outlet>`,
+})
+export class AuthenticationComponent {}
