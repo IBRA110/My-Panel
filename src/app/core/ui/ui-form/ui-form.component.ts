0e6f6f856220757a1df4ac2d3144b14096b7959f
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-07 14:19:44 +0300

    created sign-in layout

--- src/app/core/ui/ui-form/ui-form.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ui-form',
-  templateUrl: './ui-form.component.html',
-  styleUrls: ['./ui-form.component.scss'],
-})
-export class UiFormComponent {}
