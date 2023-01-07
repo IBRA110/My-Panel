59dc733d208b4e677dc96d78bd212947f6ce77a6
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-07 14:42:30 +0300

    created reactive form

--- src/app/authentication/components/sign-in/sign-in.component.ts
+++ src/app/authentication/components/sign-in/sign-in.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+export interface SignInForm {
+  userName: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent {}
+export class SignInComponent implements OnInit {
+  public form: FormGroup<SignInForm> = new FormGroup<SignInForm>({
+    userName: new FormControl<string>(''),
+    password: new FormControl<string>(''),
+  });
+
+  public ngOnInit() {
+    this.form = new FormGroup<SignInForm>({
+      userName: new FormControl<string>('', Validators.required),
+      password: new FormControl<string>('', Validators.required),
+    });
+  }
+}
--- src/app/authentication/components/sign-in/sing-in.module.ts
+++ src/app/authentication/components/sign-in/sing-in.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [SignInComponent],
 })
 export class SignInModule {}
