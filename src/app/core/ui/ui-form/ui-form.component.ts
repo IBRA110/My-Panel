745e2885b0bb1b5ffc803b1cda152c157a756b57
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-07 12:46:30 +0300

    ui-form init

---
+++ src/app/core/ui/ui-form/ui-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-form',
+  templateUrl: './ui-form.component.html',
+  styleUrls: ['./ui-form.component.scss'],
+})
+export class UiFormComponent {}
