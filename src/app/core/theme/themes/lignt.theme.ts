c0be5edba8fd78140375b51bd91b956e9a3a50bb
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-07 20:40:06 +0300

    update themes && add variables to scss

--- src/app/core/theme/themes/lignt.theme.ts
+++ src/app/core/theme/themes/lignt.theme.ts
@@ -1,8 +1,14 @@
+import { range } from 'rxjs';
 import { Theme } from '../theme.interface';
 
 export const lightTheme: Theme = {
   name: 'light',
   properties: {
     '--background-color': 'rgb(235, 237, 240)',
+    '--backgorund-transparent-color': 'rgba(235, 237, 240, 0.13)',
+    '--border-color': 'rgba(235, 237, 240, 0.1)',
+    '--box-shadow-color': 'rgba(0, 0, 0, 0.6)',
+    '--base-text-color': 'rgb(87, 87, 87)',
+    '--input-backgound-color': 'rgb(227, 228, 230)',
   },
 };
