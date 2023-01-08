0a0643c2fb5d1609c16313569cda56ce63a9226c
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-08 13:46:00 +0300

    added ui-button to authentication components

--- src/app/authentication/components/sign-in/sing-in.module.ts
+++ src/app/authentication/components/sign-in/sing-in.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UiButtonModule } from 'src/app/core/ui/ui-button/ui-button.module';
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, UiButtonModule],
   exports: [SignInComponent],
 })
 export class SignInModule {}
--- src/app/authentication/components/sign-up/sing-up.module.ts
+++ src/app/authentication/components/sign-up/sing-up.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignUpComponent } from './sign-up.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UiButtonModule } from 'src/app/core/ui/ui-button/ui-button.module';
 
 @NgModule({
   declarations: [SignUpComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, UiButtonModule],
   exports: [SignUpComponent],
 })
 export class SignUpModule {}
