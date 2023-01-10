d23489510dd6ae85c00c2e638ce8e876065bde9b
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-10 15:35:32 +0300

    registerDTO updated

--- Draw-My-Dream.API/DTOs/RegisterDTO.cs
+++ Draw-My-Dream.API/DTOs/RegisterDTO.cs
@@ -5,23 +5,15 @@ namespace API.DTOs
     public class RegisterDTO
     {
         [Required(ErrorMessage = "Username is required!")]
-        public string Username { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        
+        public string Username { get; set; }     
         [Required(ErrorMessage = "Email is required!")]
         [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", 
             ErrorMessage = "Please enter a valid e-mail address")]
         public string Email { get; set; }
-        
         [Required(ErrorMessage = "Password is required!")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", 
             ErrorMessage = 
             "Password must be stronger: Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character")]
         public string Password { get; set; }
-        public string Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
     }
 }
