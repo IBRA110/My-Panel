61ffde68e4cb9ce6fb5d0eb75b81f730a9b1ae63
Ibrahim <mamedovarbeiter@gmail.com>
2023-01-30 14:49:51 +0300

    added background image for authenticated page & fixed styles

--- src/app/core/theme/themes/lignt.theme.ts
+++ src/app/core/theme/themes/lignt.theme.ts
@@ -3,8 +3,8 @@ import { Theme } from '../theme.interface';
 export const lightTheme: Theme = {
   name: 'light',
   properties: {
-    '--background-color': 'rgba(228,225,206,255)',
-    '--backgorund-transparent-color': 'rgba(235, 237, 240, 0.13)',
+    '--background-color': 'rgba(228, 225, 206, 255)',
+    '--backgorund-transparent-color': 'rgba(241, 228, 212, 0.3)',
     '--border-color': 'rgba(235, 237, 240, 0.1)',
     '--box-shadow-color': 'rgba(0, 0, 0, 0.6)',
     '--base-text-color': 'rgb(87, 87, 87)',
