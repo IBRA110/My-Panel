dfde64cef0995770d5c9f5a6fd71b57e3063163e
Ibrahim <mamedovarbeiter@gmail.com>
2023-02-12 19:01:18 +0300

    created controller to get users with roles

--- Draw-My-Dream.API/Controllers/AdminController.cs
+++ Draw-My-Dream.API/Controllers/AdminController.cs
@@ -1,15 +1,36 @@
+using Core.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AdminController : BaseApiController
     {
+        private readonly UserManager<AppUserEntity> _userManager;
+        public AdminController(UserManager<AppUserEntity> userManager)
+        {
+            userManager = _userManager;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins can see this");
+            var users = await _userManager.Users
+                .Include(r => r.UserRoles)
+                .ThenInclude(r => r.Role)
+                .OrderBy(u => u.UserName)
+                .Select(u => new
+                {
+                    Id = u.Id,
+                    UserName = u.UserName,
+                    Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(users);
         }
 
         [Authorize(Policy = "ModeratePhotoRole")]
--- Draw-My-Dream.API/Extensions/IdentityServiceExtensions.cs
+++ Draw-My-Dream.API/Extensions/IdentityServiceExtensions.cs
@@ -32,6 +32,7 @@ namespace API.Extensions
                         ValidateAudience = false
                     };
                 });
+
             services.AddAuthorization(opt => 
             {
                 opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
