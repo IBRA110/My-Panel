0c403f7c0aa2f8433de3d49a30d968835643ffb0
Kerim Yarmakov <kyarmakov@gmail.com>
2023-03-12 19:12:20 +0300

    create dashboard module inside crm module

--- src/app/pages/crm/crm.module.ts
+++ src/app/pages/crm/crm.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { CrmRoutingModule } from './crm-routing.module';
 import { CrmComponent } from './crm.component';
+import { DashboardModule } from './pages/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [CrmComponent, SideBarComponent, NavBarComponent],
-  imports: [CommonModule, CrmRoutingModule],
+  imports: [CommonModule, CrmRoutingModule, DashboardModule],
 })
 export class CrmModule {}
