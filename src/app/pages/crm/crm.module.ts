b5ec6a519aca57f0de9838b6a808b81a4afa286d
Kerim Yarmakov <kyarmakov@gmail.com>
2023-03-12 19:15:36 +0300

    create chat module inside crm module

--- src/app/pages/crm/crm.module.ts
+++ src/app/pages/crm/crm.module.ts
@@ -4,10 +4,9 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { CrmRoutingModule } from './crm-routing.module';
 import { CrmComponent } from './crm.component';
-import { DashboardModule } from './pages/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [CrmComponent, SideBarComponent, NavBarComponent],
-  imports: [CommonModule, CrmRoutingModule, DashboardModule],
+  imports: [CommonModule, CrmRoutingModule],
 })
 export class CrmModule {}
---
+++ src/app/pages/crm/pages/chat/chat.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ChatComponent } from './chat.component';
+
+@NgModule({
+  declarations: [ChatComponent],
+  imports: [CommonModule],
+  exports: [],
+  providers: [],
+})
+export class ChatModule {}
