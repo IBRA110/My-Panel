a8b6574c63fc5744184b721e4117971025cbdf27
Kerim Yarmakov <kyarmakov@gmail.com>
2023-03-12 20:01:32 +0300

    add routerLink && changed icon for chat page

--- src/app/pages/crm/components/side-bar/side-bar.component.ts
+++ src/app/pages/crm/components/side-bar/side-bar.component.ts
@@ -9,16 +9,11 @@ import { isSidebarToggledSelector } from '../../data-access/store/crm.selectors'
   styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent implements OnInit {
-  public menuItemActive: string = 'home';
   public isSidebarToggled$: Observable<boolean>;
 
   public constructor(private _store: Store) {}
 
   public ngOnInit(): void {
     this.isSidebarToggled$ = this._store.select(isSidebarToggledSelector);
-  }
-
-  public toggleMenuItemActive(item: string): void {
-    this.menuItemActive = item;
   }
 }
