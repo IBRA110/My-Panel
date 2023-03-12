ed37da758a2d6de770a4f1d3f6eb10117517528b
Kerim Yarmakov <kyarmakov@gmail.com>
2023-03-12 22:05:18 +0300

    create drop-down-lang component inside nav-bar component

---
+++ src/app/pages/crm/components/nav-bar/components/drop-down-lang/drop-down-lang.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drop-down-lang',
+  templateUrl: './drop-down-lang.component.html',
+  styleUrls: ['./drop-down-lang.component.scss'],
+})
+export class DropdownLangComponent {}
--- src/app/pages/crm/crm.module.ts
+++ src/app/pages/crm/crm.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { DropdownLangComponent } from './components/nav-bar/components/drop-down-lang/drop-down-lang.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { CrmRoutingModule } from './crm-routing.module';
 import { CrmComponent } from './crm.component';
 
 @NgModule({
-  declarations: [CrmComponent, SideBarComponent, NavBarComponent],
+  declarations: [
+    CrmComponent,
+    SideBarComponent,
+    NavBarComponent,
+    DropdownLangComponent,
+  ],
   imports: [CommonModule, CrmRoutingModule],
 })
 export class CrmModule {}
