a9f89d0d4d63353faf80c9b99d8bc5886ef3d282
Kerim Yarmakov <kyarmakov@gmail.com>
2023-03-14 22:38:20 +0300

    add dropdown toggle functionality

--- src/app/pages/crm/components/nav-bar/components/drop-down-lang/drop-down-lang.component.ts
+++ src/app/pages/crm/components/nav-bar/components/drop-down-lang/drop-down-lang.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { filter, fromEvent, take } from 'rxjs';
 
 @Component({
   selector: 'app-drop-down-lang',
@@ -8,12 +9,35 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class DropdownLangComponent {
   public selectedLang: string = 'EN';
+  public isDropdownShown: boolean = false;
 
-  public constructor(private _translate: TranslateService) {}
+  public constructor(
+    private _translate: TranslateService,
+    private _el: ElementRef,
+  ) {}
 
   public translateTo(lang: string): void {
     this._translate.use(lang);
-
     this.selectedLang = lang.substring(0, 2).toUpperCase();
+  }
+
+  public toggleDropdown(): void {
+    if (!this.isDropdownShown) {
+      fromEvent(document, 'click')
+        .pipe(
+          filter((event) => {
+            return (
+              !this._el.nativeElement ||
+              !this._el.nativeElement.contains(event.target as HTMLElement)
+            );
+          }),
+          take(1),
+        )
+        .subscribe(() => {
+          this.isDropdownShown = false;
+        });
+    }
+
+    this.isDropdownShown = !this.isDropdownShown;
   }
 }
--- src/app/pages/crm/data-access/store/crm.actions.ts
+++ src/app/pages/crm/data-access/store/crm.actions.ts
@@ -1,4 +1,4 @@
-import { createAction, props } from '@ngrx/store';
+import { createAction } from '@ngrx/store';
 import { CrmActionsEnum } from './crm.enum';
 
 export const toggleSidebar = createAction(CrmActionsEnum.TOGGLE_SIDEBAR);
