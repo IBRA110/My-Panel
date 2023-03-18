2b831015f0f46097dde81f9222aef848424c459f
Ibrahim <mamedovarbeiter@gmail.com>
2023-03-18 21:18:08 +0300

    features: added Ulid to string Converter

--- Draw-My-Dream.Infrastructure/Data/DataContext.cs
+++ Draw-My-Dream.Infrastructure/Data/DataContext.cs
@@ -145,4 +145,16 @@ namespace Infrastructure.Data
         {
         }
     }
+    public class UlidToStringConverter : ValueConverter<Ulid, string>
+    {
+        private static readonly ConverterMappingHints defaultHints = new ConverterMappingHints(size: 26);
+
+        public UlidToStringConverter(ConverterMappingHints mappingHints = null)
+            : base(
+                    convertToProviderExpression: x => x.ToString(),
+                    convertFromProviderExpression: x => Ulid.Parse(x),
+                    mappingHints: defaultHints.With(mappingHints))
+        {
+        }
+    }
 }
