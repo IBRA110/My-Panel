692198589826982a2711d73951dbdfb7d56e6bf5
Ibrahim <mamedovarbeiter@gmail.com>
2023-03-24 20:18:17 +0300

    refactoring: added light colors && updated styles

--- src/app/core/theme/themes/lignt.theme.ts
+++ src/app/core/theme/themes/lignt.theme.ts
@@ -3,14 +3,13 @@ import { Theme } from '../theme.interface';
 export const lightTheme: Theme = {
   name: 'light',
   properties: {
-    '--background-color': 'rgb(21, 26, 48)',
-    '--box-shadow-color': 'rgb(26, 31, 51)',
+    '--background-color': 'rgb(237, 245, 252)',
+    '--box-shadow-color': 'rgb(199, 207, 214)',
 
-    '--nav-bar-background-color': 'rgb(34, 43, 69)',
-    '--side-bar-background-color': 'rgb(34, 43, 69)',
-    '--settings-background-color': 'rgb(34, 43, 69)',
+    '--nav-bar-background-color': 'rgb(255, 255, 255)',
+    '--side-bar-background-color': 'rgb(255, 255, 255)',
 
-    '--base-text-color': 'rgb(227, 228, 230)',
+    '--base-text-color': 'rgb(74, 73, 73)',
 
     '--button-background-default-color': 'rgb(51, 102, 255)',
     '--button-outline-basic-background-color': 'rgb(143, 155, 179)',
