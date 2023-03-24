3b0b74a4b353624aa1898892e011328bc87fa191
Ibrahim <mamedovarbeiter@gmail.com>
2023-03-24 19:51:23 +0300

    refactoring: returned alert colors

--- src/app/core/theme/themes/lignt.theme.ts
+++ src/app/core/theme/themes/lignt.theme.ts
@@ -14,5 +14,10 @@ export const lightTheme: Theme = {
 
     '--button-background-default-color': 'rgb(51, 102, 255)',
     '--button-outline-basic-background-color': 'rgb(143, 155, 179)',
+
+    '--success': 'rgb(46, 178, 59)',
+    '--error': 'rgb(255, 0, 0)',
+    '--warning': 'rgb(252, 152, 3)',
+    '--information': 'rgb(12, 136, 249)',
   },
 };
