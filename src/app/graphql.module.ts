2f4d6c4a9ed6e26355094772da63b24f0e422a3e
Ibrahim <mamedovarbeiter@gmail.com>
2023-03-24 11:40:51 +0300

    features: changed graphq url

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -8,7 +8,7 @@ import {
 } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 
-const uri = 'https://localhost:5000/graphql/';
+const uri = 'http://195.135.253.235/graphql/';
 
 const activityMiddleware = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem('accessToken');
