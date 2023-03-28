e20987c2cc7d7cfffa620cc91d7e3856ecb413b5
Ibrahim <mamedovarbeiter@gmail.com>
2023-03-28 12:57:07 +0300

    features: added active button color && added box shadow for auth components

--- src/app/core/theme/themes/lignt.theme.ts
+++ src/app/core/theme/themes/lignt.theme.ts
@@ -10,8 +10,10 @@ export const lightTheme: Theme = {
     '--side-bar-background-color': 'rgb(255, 255, 255)',
 
     '--base-text-color': 'rgb(74, 73, 73)',
+    '--button-text-color': 'rgb(237, 245, 252)',
 
     '--button-background-default-color': 'rgb(51, 102, 255)',
+    '--button-background-active-color': 'rgb(35, 62, 196)',
     '--button-outline-basic-background-color': 'rgb(143, 155, 179)',
 
     '--success': 'rgb(46, 178, 59)',
