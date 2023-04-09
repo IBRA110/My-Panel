7b8457afdfd29e468484b65c08036615ae384dfa
Ibrahim <mamedovarbeiter@gmail.com>
2023-04-10 01:10:43 +0300

    features: updated updateUserMutation && removed uploadPhotoMutation

--- backend/API/GraphQL/Users/UsersMutations.cs
+++ backend/API/GraphQL/Users/UsersMutations.cs
@@ -14,19 +14,53 @@ namespace API.GraphQL.Users
         [Authorize]
         public async Task<MemberUpdateDTO> UpdateUser(
             [Service] IUnitOfWork unitOfWork,
-            ClaimsPrincipal claimsPrincipal,
             [Service] IMapper mapper,
+            ClaimsPrincipal claimsPrincipal,
             string city,
             string country,
             DateTime dateOfBirth,
             string firstName,
             string lastName,
             string interests,
-            string introduction
+            string introduction,
+            IFile file
             )
         {
             AppUserEntity user = await unitOfWork.userRepository.GetUserByIdAsync(claimsPrincipal.FindFirst("Id").Value);
+            
+            string imagePath = "";
 
+            if (file != null)
+            {
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.Name;
+
+                ImageEntity photo = new ImageEntity
+                {
+                    Url = "images/" + uniqueFileName
+                };
+
+                if (user.Images.Count == 0)
+                {
+                    photo.IsMain = true;
+                }
+                else
+                {
+                    ImageEntity currentMain = user.Images.FirstOrDefault(x => x.IsMain);
+
+                    if (currentMain != null)
+                    {
+                        currentMain.IsMain = false;
+                    }
+                    photo.IsMain = true;
+                }
+
+                user.Images.Add(photo);
+                
+                imagePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/", uniqueFileName);
+                
+                await file.CopyToAsync(new FileStream(imagePath, FileMode.Create));
+            }
+            
             MemberUpdateDTO memberUpdateDTO = new MemberUpdateDTO
             {
                 City = city,
@@ -49,6 +83,7 @@ namespace API.GraphQL.Users
 
             return new MemberUpdateDTO
             {
+                PhotoUrl = imagePath,
                 City = user.City,
                 Country = user.Country,
                 DateOfBirth = user.DateOfBirth,
@@ -57,57 +92,6 @@ namespace API.GraphQL.Users
                 Interests = user.Interests,
                 Introduction = user.Introduction
             };
-
         }
-        
-        [UseProjection]
-        [Authorize]
-        public async Task<ImageUpdateDTO> UploadUserImage(
-            [Service] IUnitOfWork unitOfWork,
-            ClaimsPrincipal claimsPrincipal,
-            IFile file)
-        {
-            AppUserEntity user = await unitOfWork.userRepository.GetUserByIdAsync(claimsPrincipal.FindFirst("Id").Value);
-            
-            string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.Name;
-
-            ImageEntity photo = new ImageEntity
-            {
-                Url = "images/" + uniqueFileName
-            };
-
-            if (user.Images.Count == 0)
-            {
-                photo.IsMain = true;
-            }
-            else 
-            {
-                ImageEntity currentMain = user.Images.FirstOrDefault(x => x.IsMain);
-
-                if (currentMain != null)
-                {
-                    currentMain.IsMain = false;
-                }
-                photo.IsMain = true;
-            }
-
-            user.Images.Add(photo);
-            
-            if (!await unitOfWork.Complete())
-            {
-                throw new GraphQLException("Failed to update user");
-            }
-
-            string imagePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/", uniqueFileName);
-            await file.CopyToAsync(new FileStream(imagePath, FileMode.Create));
-
-
-            return new ImageUpdateDTO
-            {
-                Url = photo.Url
-            };
-
-        }
-
     }
 }
--- backend/Core/DTOs/MemberUpdateDTO.cs
+++ backend/Core/DTOs/MemberUpdateDTO.cs
@@ -2,6 +2,7 @@ namespace Core.DTOs
 {
     public class MemberUpdateDTO
     {
+        public string? PhotoUrl { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
