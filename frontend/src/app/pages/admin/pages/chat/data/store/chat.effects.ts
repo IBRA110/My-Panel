2792a9fd5cc766ab684b85661d93d2038f347131
Ibrahim <mamedovarbeiter@gmail.com>
2023-04-15 22:03:46 +0300

    features: added logic to display online users in chat

--- frontend/src/app/pages/admin/pages/chat/data/store/chat.effects.ts
+++ frontend/src/app/pages/admin/pages/chat/data/store/chat.effects.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
 import { loadUsers, loadUsersFaled, loadUsersSuccess } from './chat.actions';
 import { catchError, map, of, switchMap } from 'rxjs';
 import { ChatService } from '../services/chat.service';
 import { ChatUsers } from '../interfaces/users.interface';
+import { Store } from '@ngrx/store';
+import { selectOnlineUsers } from 'src/app/pages/admin/data/store/admin.selectors';
 
 @Injectable()
 export class ChatEffects {
   public constructor(
     private actions$: Actions,
     private chatService: ChatService,
+    private store: Store,
   ) {}
 
   private loadUserEffect$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(loadUsers),
-      switchMap((action) => {
+      concatLatestFrom(() => [this.store.select(selectOnlineUsers)]),
+      switchMap(([action, onlineUsers]) => {
         return this.chatService.getUsers(action).pipe(
           map((data) => {
             const users: ChatUsers[] = data.data.users.map((u) => {
@@ -26,7 +30,7 @@ export class ChatEffects {
                 lastActive: u.lastActive,
                 photoUrl: u.photoUrl,
                 lastName: u.lastActive,
-                isOnline: false,
+                isOnline: onlineUsers.some((id) => id === u.id),
               };
             });
             return loadUsersSuccess({ users: users });
--- frontend/src/app/pages/admin/pages/chat/data/store/chat.reduser.ts
+++ frontend/src/app/pages/admin/pages/chat/data/store/chat.reduser.ts
@@ -2,6 +2,10 @@ import { Action, on } from '@ngrx/store';
 import { ChatState, initialState } from './chat.state';
 import { createRehydrateReducer } from 'src/app/core/reducers/rehydrate-reducer';
 import { loadUsersSuccess } from './chat.actions';
+import {
+  getOnlineUser,
+  removeOfflineUser,
+} from 'src/app/pages/admin/data/store/admin.actions';
 
 export const chatFeatureKey = 'chat';
 
@@ -11,6 +15,44 @@ const chatReducer = createRehydrateReducer(
   on(loadUsersSuccess, (state, users) => ({
     ...state,
     users: users.users,
+  })),
+  on(getOnlineUser, (state, { user }) => ({
+    ...state,
+    users: [
+      ...state.users.map((element) => {
+        if (element.id === user) {
+          return {
+            id: element.id,
+            firstName: element.firstName,
+            lastName: element.lastName,
+            lastActive: element.lastActive,
+            photoUrl: element.photoUrl,
+            userName: element.userName,
+            isOnline: true,
+          };
+        }
+        return element;
+      }),
+    ],
+  })),
+  on(removeOfflineUser, (state, { user }) => ({
+    ...state,
+    users: [
+      ...state.users.map((element) => {
+        if (element.id === user) {
+          return {
+            id: element.id,
+            firstName: element.firstName,
+            lastName: element.lastName,
+            lastActive: element.lastActive,
+            photoUrl: element.photoUrl,
+            userName: element.userName,
+            isOnline: false,
+          };
+        }
+        return element;
+      }),
+    ],
   })),
 );
 
