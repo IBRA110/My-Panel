04c01ae3d057bc46213997885bf0213342b9776f
Ibrahim <mamedovarbeiter@gmail.com>
2023-04-16 20:39:32 +0300

    features: created search users form

--- frontend/src/app/pages/admin/pages/chat/chat.module.ts
+++ frontend/src/app/pages/admin/pages/chat/chat.module.ts
@@ -6,11 +6,13 @@ import { StoreModule } from '@ngrx/store';
 import { chatFeatureKey, reducer } from './data/store/chat.reduser';
 import { EffectsModule } from '@ngrx/effects';
 import { ChatEffects } from './data/store/chat.effects';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ChatComponent],
   imports: [
     CommonModule,
+    TranslateModule,
     RouterModule.forChild([{ path: '', component: ChatComponent }]),
     StoreModule.forFeature(chatFeatureKey, reducer),
     EffectsModule.forFeature([ChatEffects]),
