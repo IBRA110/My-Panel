57fc8e2509197337e2eb799e48ac6fe02ae058d8
Ibrahim <mamedovarbeiter@gmail.com>
2023-06-18 17:00:41 +0300

    features: created layout & logic for change color theme

--- frontend/src/app/core/components/my-settings/components/display/display.component.ts
+++ frontend/src/app/core/components/my-settings/components/display/display.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ThemesEnum } from 'src/app/core/enums/themes.enum';
+import { ThemeService } from 'src/app/core/services/theme.service';
 
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss']
+  styleUrls: ['./display.component.scss'],
 })
 export class DisplayComponent {
+  public constructor(private themeService: ThemeService) {}
 
+  public setTheme(name: string): void {
+    this.themeService.setTheme(name);
+  }
+
+  public get theme(): typeof ThemesEnum {
+    return ThemesEnum;
+  }
 }
