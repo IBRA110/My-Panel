b37104eaa7c0d7a311fe1988bea83b008a0571ff
Ibrahim <mamedovarbeiter@gmail.com>
2023-06-19 22:35:43 +0300

    features: added dark theme colors

--- frontend/src/app/core/themes/dark.theme.ts
+++ frontend/src/app/core/themes/dark.theme.ts
@@ -5,15 +5,17 @@ export const darkTheme: Theme = {
   properties: {
     '--background-color': 'rgb(21, 26, 48)',
     '--base-backgound-color': 'rgb(34, 43, 69)',
-    '--box-shadow-color': 'rgb(26, 31, 51)',
+    '--box-shadow-color': 'rgb(25, 31, 57)',
 
     '--base-text-color': 'rgb(227, 228, 230)',
+    '--transparent-text-color': 'rgba(0, 48, 93, 0.5)',
     '--button-text-color': 'rgb(237, 245, 252)',
 
-    '--button-background-default-color': 'rgb(1, 30, 86)',
+    '--button-background-default-color': 'rgb(14, 125, 22)',
+    '--button-background-active-color': 'rgb(2, 43, 110)',
     '--button-outline-basic-background-color': 'rgb(143, 155, 179)',
 
-    '--select-tab-line': 'rgb(8, 203, 57)',
+    '--selected-tab-color': 'rgba(199, 207, 214, 0.5)',
 
     '--success': 'rgb(46, 178, 59)',
     '--error': 'rgb(255, 0, 0)',
