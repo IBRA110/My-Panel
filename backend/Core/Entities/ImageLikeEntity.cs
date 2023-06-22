e1654da2dea7629662107972c5b2b5eb74320c9c
Ibrahim <mamedovarbeiter@gmail.com>
2023-06-22 17:09:37 +0300

    refactoring: removed likes Entity

--- backend/Core/Entities/ImageLikeEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Entities
-{
-    public class ImageLikeEntity
-    {
-        public string LikedUserId { get; set; }
-        public string LikedImageId { get; set; }
-        public ImageEntity Image { get; set; }
-    }
-}
